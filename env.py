9afb4feb86ee11e5fa6a3b25038a3d625c19ea96
unstanbul <101056233+unstanbul@users.noreply.github.com>
2023-12-31 00:30:41 +0300

    commit

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st  # use on Terminal as: streamlit run yz_streamlit.py
 from dotenv import load_dotenv
 
@@ -6,8 +7,11 @@ load_dotenv()
 
 header_container = st.container()
 
+name = os.environ["name"]
+
 
 with header_container:
     # Use Markdown for Writing Web Text
     st.title("Hello World :wave:")
     st.write("Welcome to Python Streamlit")
+    st.write(name)
